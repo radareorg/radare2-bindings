radareorg/radare2

Added Java JNI install support

r2-bindings/do-swig.sh
@@ -6,6 +6,7 @@
 LNG=$1
 MOD=$2
 VALABINDFLAGS="" ; [ 1 = "${DIST}" ] && VALABINDFLAGS="-C"
+RADARE_JPKG_DIR=org/radare/radare2
 if [ -z "${MOD}" ]; then
 	echo "Usage: ./libr-swig.sh [python] [r_foo]"
 	exit 1
@@ -21,6 +22,10 @@ case "${LNG}" in
 *node*|ctypes)
 	VBMODE=''
 	;;
+java)
+        export SWIGFLAGS="-package org.radare.radare2 -outdir ${RADARE_JPKG_DIR}"
+        VBMODE=--swig
+        ;;
 *)
 	VBMODE=--swig
 	;;

r2-bindings/rules.mk
@@ -29,6 +29,9 @@ ifeq (${LANG},dlang)
 	echo "MOD=$$mod" ; \
 	valabind --dlang -N Radare -m $$mod --vapidir=../vapi $$mod
 else
+ifeq (${LANG},java)
+	mkdir -p ${RADARE_JPKG_DIR}
+endif
 	@-test ../vapi/`echo $@|sed -e s,.${SOEXT},.vapi,` -nt ${LIBS_PFX}$@ ; \
 	if [ ! $$? = 0 ]; then \
 	  if [ ! -e ${LIBS_PFX}$@ ]; then \
@@ -50,6 +53,10 @@ install:
 	cd .. ; ${MAKE} install-${LANG}
 
 clean:
+ifeq (${LANG},java)
+	rm -rf radare2.jar
+	rm -rf ${RADARE_JPKG_DIR}
+endif
 ifneq ($(SAVED),)
 	mkdir -p .skip
 	cp $(SAVED) .skip

radareorg/radare2-bindings

do-swig.sh
@@ -6,6 +6,7 @@
 LNG=$1
 MOD=$2
 VALABINDFLAGS="" ; [ 1 = "${DIST}" ] && VALABINDFLAGS="-C"
+R2_JAVA_DIR=org/radare/radare2
 if [ -z "${MOD}" ]; then
 	echo "Usage: ./libr-swig.sh [python] [r_foo]"
 	exit 1
@@ -21,6 +22,10 @@ case "${LNG}" in
 *node*|ctypes)
 	VBMODE=''
 	;;
+java)
+        export SWIGFLAGS="-package org.radare.radare2 -outdir ${R2_JAVA_DIR}"
+        VBMODE=--swig
+        ;;
 *)
 	VBMODE=--swig
 	;;

rules.mk
@@ -29,6 +29,9 @@ ifeq (${LANG},dlang)
 	echo "MOD=$$mod" ; \
 	valabind --dlang -N Radare -m $$mod --vapidir=../vapi $$mod
 else
+ifeq (${LANG},java)
+	mkdir -p ${R2_JAVA_DIR}
+endif
 	@-test ../vapi/`echo $@|sed -e s,.${SOEXT},.vapi,` -nt ${LIBS_PFX}$@ ; \
 	if [ ! $$? = 0 ]; then \
 	  if [ ! -e ${LIBS_PFX}$@ ]; then \
@@ -50,6 +53,10 @@ install:
 	cd .. ; ${MAKE} install-${LANG}
 
 clean:
+ifeq (${LANG},java)
+	rm -rf radare2.jar
+	rm -rf ${R2_JAVA_DIR}
+endif
 ifneq ($(SAVED),)
 	mkdir -p .skip
 	cp $(SAVED) .skip