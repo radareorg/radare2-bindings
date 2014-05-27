50d6f36cc747687895f10757542731b868e49836
Willi Ballenthin <wilbal1087@gmail.com>
2014-05-26 20:43:55 -0400

    Update RBin API calls in Go bindings test

--- go/test-r_bin.go
+++ go/test-r_bin.go
@@ -12,12 +12,12 @@ func main() {
 		os.Exit (1)
 	}
 	b := r_bin.NewRBin ()
-	b.Load (os.Args[1], false)
+	b.Load (os.Args[1], 0, 0, 0, 0, 0)
 	baddr := b.Get_baddr ()
 	fmt.Println ("-> Sections")
 	fmt.Printf ("baddr=%08x\n", baddr)
 	for _, s:= range b.Get_sections () {
 		fmt.Printf ("offset=0x%08x va=0x%08x size=%05d %s\n",
-				s.GetOffset(), baddr+s.GetRva(), s.GetSize(), s.GetName())
+				s.GetPaddr(), baddr+s.GetVaddr(), s.GetSize(), s.GetName())
 	}
 }
