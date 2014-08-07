0c814adf7c03ede4eb9998c453b837cb12709960
Anton Kochkov <anton.kochkov@gmail.com>
2014-08-07 04:26:21 +0400

    Fix Go directory search

--- go/goenv.sh
+++ go/goenv.sh
@@ -22,7 +22,12 @@ GOPACK="go tool pack"
 GOOS=`uname | tr 'A-Z' 'a-z'`
 if ! "$GOROOT"; then
 	echo "Warning, setting \$GOROOT to '/usr/lib/go', but this should probably be set elsewhere";
-	GOROOT=/usr/lib/go;
+	if [ -d "/usr/lib/go" ]; then
+		GOROOT=/usr/lib/go;
+	fi
+	if [ -d "/usr/lib64/go" ]; then
+		GOROOT=/usr/lib64/go;
+	fi
 else
 	GOROOT="$GOROOT";
 fi
