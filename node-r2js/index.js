845737ef674a136a1193d9654cf6a5a8e638b1cf
pancake <pancake@nopcode.org>
2014-08-08 14:11:54 +0200

    Update NodeJS r2 generic api test program and api

--- node-r2js/index.js
+++ node-r2js/index.js
@@ -22,88 +22,68 @@ function remoteCmd(port, cmd, cb) {
 	}
 }
 
-var host = "cloud.rada.re"
 
-function httpCmd(port, cmd, cb) {
-// TODO
-	var options = {
-		hostname: host,
-		port: 80,
-		path: '/cmd/'+cmd, // encode
-		method: 'GET'
-	};
-
+function httpCmd(uri, cmd, cb) {
 	var text = "";
-	var req = http.get('http://'+host+'/cmd/'+cmd, function (res) {
-		console.log("Got response: " + res.statusCode);
-		console.log ("===",res.output);
-console.log ("===",res);
-		text += res.output;
-	}).on ("errro", function(res) {
+	var req = http.get(uri+cmd, function (res) {
+		res.on("data",function(res) {
+			text += res;
+		});
+	}).on ("error", function(res) {
 		  console.log("Got response: " + res.statusCode);
-	}).on("data",function(res) {
-		text += res;
-console.log ("===",res);
 	});
-
 	req.on('close', function(e) {
-		console.log("closed");
 		cb (text);
 	});
+}
 
-	// write data to request body
-	/*
-	   req.write('data\n');
-	   req.write('data\n');
-	   req.end();
-	 */
+function r2bind(file, cb, r2cmd) {
+	var port = (file.indexOf("http") != -1)? file
+		: (4000+(Math.random()*4000))|0; // TODO must be random
+	var ls = process.spawn('r2', ["-qc.:"+port, file]);
+	var running = false;
+	var r2 = {
+		cmd : function (s, cb2) { r2cmd (port, s, cb2); },
+		quit: function() { ls.kill ('SIGINT'); }
+	}
+	ls.stderr.on('data', function (data) {
+		if (!running) {
+			running = true;
+			cb (r2);
+		}
+		//console.log('stdout: ' + data);
+	});
+	ls.stdout.on('data', function (data) {
+		if (!running) {
+			running = true;
+			cb (r2);
+		}
+		//console.log('stderr: ' + data);
+	});
+	ls.on('error', function (code) {
+		running = false;
+		console.log('ERROR');
+	});
+	ls.on('close', function (code) {
+		running = false;
+		if (code != 0)
+			console.log('child process exited with code ' + code);
+	});
 }
 
 var r2node = {
+	// TODO
+	// - listen()
+	// - rap()
 	connect : function (uri, cb) {
-			  // use http server uri /cmd/...
-		  },
-	launch : function(file, cb) {
-			 var port = (4000+(Math.random()*4000))|0; // TODO must be random
-			 var ls = process.spawn('r2', ["-nqc.:"+port, file]);
-			 var running = false;
-			 var r2 = {
-				 cmd : function (s, cb2) {
-					       //httpCmd(port, s, cb2);
-					       remoteCmd (port, s, cb2);
-				       },
-				 quit: function() {
-					       ls.kill ('SIGINT');
-				       }
-			 }
-			 ls.stderr.on('data', function (data) {
-				 if (!running) {
-					 running = true;
-					 cb (r2);
-				 }
-				 //console.log('stdout: ' + data);
-			 });
-			 ls.stdout.on('data', function (data) {
-				 if (!running) {
-					 running = true;
-					 cb (r2);
-				 }
-				 //console.log('stderr: ' + data);
-			 });
-			 ls.on('error', function (code) {
-				 running = false;
-				 console.log('ERROR');
-			 });
-			 ls.on('close', function (code) {
-				 running = false;
-				 if (code != 0)
-					 console.log('child process exited with code ' + code);
-			 });
-		 },
+		r2bind (uri, cb, httpCmd);
+	},
+	launch : function (file, cb) {
+		r2bind (file, cb, remoteCmd);
+        },
 	listen: function(file, cb) {
-			// TODO
-		}
+		// TODO
+	}
 }
 
 module.exports = r2node;
-
--- node-r2js/test.js
+++ node-r2js/test.js
@@ -1,8 +1,27 @@
+/* Common-Javascript API for R2  --pancake 2014 */
 
-var r2node = require ("./"); //r2node.js");
-r2node.launch ("/bin/ls", function(r2) {
-	r2.cmd ("pd 3", function (o) {
-		console.log (o);
-		r2.quit ()
+/* This is a NodeJS program that uses the generic r2 api
+   which is also compatible with the WebUI and Duktape.
+   Enabling you to write Javascript extensions for r2
+   that run in the shell, the web or inside r2 */
+
+/* require the nodejs api */
+var r2jsapi = "./r2.js";
+var r2node = require ("./");
+
+function doSomeStuff(r) {
+	var r2 = require (r2jsapi)(r);
+	r2.analOp ("entry0", function(op) {
+		console.log (op.size, op.opcode, op.esil);
 	});
-});
+
+	r2.cmd ('af@entry0', function (o) {
+		r2.cmd ("pdf @ entry0", function (o) {
+			console.log (o);
+			r.quit ()
+		});
+	});
+}
+
+r2node.connect ("http://cloud.rada.re/cmd/", doSomeStuff);
+r2node.launch ("/bin/ls", doSomeStuff);
