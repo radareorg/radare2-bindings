ded02d6d2d895754342a79a99902eecee0797480
pancake <pancake@nopcode.org>
2015-06-02 18:48:01 +0200

    Remove r_diff references

--- libs.mk
+++ libs.mk
@@ -8,7 +8,7 @@ LIBS+=r_asm.${SOEXT}
 #LIBS=r_util.${SOEXT}
 #WIP=1
 #ifeq (${WIP},1)
-#LIBS=r_util.${SOEXT} r_bp.${SOEXT} r_asm.${SOEXT} r_diff.${SOEXT}
+#LIBS=r_util.${SOEXT} r_bp.${SOEXT} r_asm.${SOEXT}
 #LIBS+=r_bin.${SOEXT} r_cons.${SOEXT} r_anal.${SOEXT} r_cmd.${SOEXT}
 #LIBS+=r_debug.${SOEXT} r_config.${SOEXT} r_io.${SOEXT} r_syscall.${SOEXT}
 #LIBS+=r_search.${SOEXT} r_lib.${SOEXT} r_flags.${SOEXT} r_fs.${SOEXT}
