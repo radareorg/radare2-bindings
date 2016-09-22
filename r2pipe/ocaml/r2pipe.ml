bd7fccdf5fcd4e1e801ec764a646a98b1969b0d9
Jeong Jihoon <binoopang@gmail.com>
2016-09-22 11:44:16 +0900

    ready to opam

--- r2pipe/ocaml/r2pipe.ml
+++ r2pipe/ocaml/r2pipe.ml
@@ -1,54 +1,63 @@
 (* 
  * R2PIPE 
  *
- * This module (will) provides an API to interact with the radare2
- * commandline interface from Python using a pipe
- *
- * http://pleac.sourceforge.net/pleac_ocaml/processmanagementetc.html
+ * This module provides an API to interact with the radare2
+ * commandline interface from OCaml using a pipe.
  *
  *)
 
-open Unix
-       
-type file_scheme = FILE_LOCAL | FILE_REMOTE
-type local_ctx = in_channel * out_channel * in_channel
+type ctx =
+  | Local of in_channel * out_channel * in_channel
+  | Remote of string (* For Future *)
 
 let bytes_startswith s sub =
-  let slen = Bytes.length s in
-  let sublen = Bytes.length sub in
+  let slen, sublen = (Bytes.length s, Bytes.length sub) in
   if sublen>slen then false
   else if Bytes.sub_string s 0 sublen = sub then true
   else false
                                   
-let get_file_scheme f =
-  match (bytes_startswith f "http://") with
-  | true -> FILE_REMOTE
-  | false -> FILE_LOCAL
-
 let ropen f =
-  match (get_file_scheme f) with
-  | FILE_LOCAL ->
-     let (cout, cin, cerr) = open_process_full f [| |] in
-     Some(cout, cin, cerr)
-  | FILE_REMOTE -> None
+  if bytes_startswith f "http://" = false
+  then let cmd_str = Printf.sprintf "r2 -q0 %s" f in (* -q0 : be quite and print zero *)
+       let (cout, cin, cerr) = Unix.open_process_full cmd_str [| |] in
+       let _ = input_byte cout in (* Read first 0x00 byte after the radare2 init *)
+       Some(Local(cout, cin, cerr))
+  else None (* Currently, we only support Local PIPE *)
+
+let takeUntil cha ch =
+  let flag = ref true in
+  let idx = ref 0 in
+  let output = Bytes.create 65535 in
+  let () = while !flag <> false do
+             let b = input_char cha in
+             if b = ch then flag := false
+             else Bytes.set output !idx b ; idx := !idx+1
+           done
+  in
+  Bytes.sub_string output 0 (!idx-1)
 
 let cmd ctx c =
-  let cout, cin, cerr = ctx in
-  let () = output_string cin c in
-  let cmd_out_descr = Unix.descr_of_in_channel cout in
-  let cmd_err_descr = Unix.descr_of_in_channel cerr in
-  let selector = ref [cmd_err_descr ; cmd_out_descr] in
-  while !selector <> [] do
-    let can_read, _, _ = Unix.select !selector [] [] 1.0 in
-    List.iter
-      (fun fh ->
-        try
-          if fh = cmd_err_descr
-          then Printf.printf "%s" (input_line cerr)
-          else Printf.printf "%s" (input_line cout)
-        with End_of_file ->
-          selector := List.filter (fun fh' -> fh <> fh') !selector)
-      can_read
-  done;
+  match ctx with
+  | Local (cout, cin, cerr) -> 
+     let cmd_str = Printf.sprintf "%s\n" c in
+     let () = output_string cin cmd_str in
+     let () = flush_all () in
+     takeUntil cout '\x00'
+  | _ -> ""
 
-  
+let cmdj ctx c = 
+  Yojson.Safe.from_string (cmd ctx c)
+
+(* ------------ UNIT TEST -------------- *)
+(*           
+let opt_get = function
+  | Some x -> x
+  | None -> raise (Invalid_argument "Opt_get")
+                  
+let () =
+  let ctx = opt_get (ropen "/bin/ls") in
+  let output = cmd ctx "pd 2" in
+  let () = print_endline output in
+  let output = cmdj ctx "pdj 2" in
+  print_endline (Yojson.Safe.pretty_to_string output)
+*)
