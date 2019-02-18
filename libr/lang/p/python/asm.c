fe74494b1ce95b2b7e892800765c01bbd4ed06cf
Florian Märkl <info@florianmaerkl.de>
2019-02-18 21:56:34 +0100

    Minor Fixes for Python r2lang asm (#210)

--- libr/lang/p/python/asm.c
+++ libr/lang/p/python/asm.c
@@ -4,22 +4,45 @@
 static void *py_assemble_cb = NULL;
 static void *py_disassemble_cb = NULL;
 
+static int check_list_result(PyObject *result, const char *fcn_name) {
+	if (!result) {
+		eprintf ("Error while calling %s in Python\n", fcn_name);
+		PyErr_Print ();
+		return 0;
+	}
+	if (!PyList_Check (result)) {
+		PyObject *str = PyObject_Str (result);
+		Py_DECREF (result);
+		if (!str) {
+			PyErr_Print ();
+		} else {
+			if (PyUnicode_Check (str)) {
+				eprintf ("Unknown type returned from %s. List was expected, got %s.\n", fcn_name, PyUnicode_AsUTF8 (str));
+			} else {
+				eprintf ("Unknown type returned from %s. List was expected.\n", fcn_name);
+			}
+			Py_DECREF (str);
+		}
+		return 0;
+	}
+	return 1;
+}
+
 static int py_assemble(RAsm *a, RAsmOp *op, const char *str) {
 	int i, size = 0;
 	int seize = -1;
 	const char *opstr = str;
-        ut8 *buf = (ut8*)r_strbuf_get (&op->buf);
+	ut8 *buf = (ut8*)r_strbuf_get (&op->buf);
 	if (py_assemble_cb) {
 		PyObject *arglist = Py_BuildValue ("(z)", str);
 		PyObject *result = PyEval_CallObject (py_assemble_cb, arglist);
-		if (result && PyList_Check (result)) {
+		if (check_list_result (result, "assemble")) {
 			seize = size = PyList_Size (result);
 			for (i = 0; i < size ; i++) {
 				PyObject *len = PyList_GetItem (result, i);
 				buf[i] = PyNumber_AsSsize_t (len, NULL);
 			}
-		} else {
-			eprintf ("Unknown type returned. List was expected.\n");
+			Py_DECREF (result);
 		}
 	}
 	op->size = size = seize;
@@ -33,15 +56,14 @@ static int py_disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 	int seize = -1;
 	const char *opstr = "invalid";
 	if (py_disassemble_cb) {
-		PyObject *arglist = Py_BuildValue ("("BYTES_FMT")", buf, len, a->pc);
+		PyObject *arglist = Py_BuildValue ("(y#K)", buf, len, a->pc);
 		PyObject *result = PyEval_CallObject (py_disassemble_cb, arglist);
-		if (result && PyList_Check (result)) {
+		if (check_list_result (result, "assemble")) {
 			PyObject *len = PyList_GetItem (result, 0);
 			PyObject *str = PyList_GetItem (result, 1);
 			seize = PyNumber_AsSsize_t (len, NULL);
 			opstr = PySTRING_ASSTRING (str);
-		} else {
-			eprintf ("Unknown type returned. List was expected.\n");
+			Py_DECREF (result);
 		}
 	}
 	r_asm_op_init (op);
