09920b33dc3926beaf70cd058b44ab91b389afbb
pancake <pancake@nopcode.org>
2021-07-28 12:37:26 +0200

    RAnal example working

--- rust/analrs/src/lib.rs
+++ rust/analrs/src/lib.rs
@@ -15,8 +15,9 @@ mod radare2;
 
 use radare2::*;
 
-const MY_NAME : *const c_char = b"anal-rs\0" as *const [u8] as *const c_char;
-const R2_VERSION: &'static [u8] = b"1.7.0-git\0";
+const MY_NAME: &'static [u8] = b"v850.rs\0";
+const MY_ARCH: &'static [u8] = b"v850\0";
+const R2_VERSION: &'static [u8] = b"5.4.0-git\0";
 const MY_DESC : &'static [u8] = b"Analysis plugin\0";
 const MY_LICENSE : &'static [u8] = b"MIT\0";
 const MY_VERSION : &'static [u8] = b"0.1.0\0";
@@ -33,6 +34,15 @@ fn analyze_binary (core: *mut c_void) -> c_int {
     return 1;
 }
 
+extern "C" fn _anal_op(_user: *mut c_void, _op: *mut RAnalOp, _addr: u64, _data: *mut c_void, _len: usize, _mask: usize) -> c_int {
+unsafe {
+	(*_op).mnemonic = libc::strdup("rustop r0, 123\0".as_ptr() as *const i8);
+	(*_op).size = 4;
+	(*_op)._type = R_ANAL_OP_TYPE_NOP;
+}
+	return 4;
+}
+
 extern "C" fn _anal_call (user: *mut c_void, input: *const c_char) -> c_int {
     let c_str: &CStr = unsafe { CStr::from_ptr(input) };
     let bytes = c_str.to_bytes();
@@ -44,22 +54,80 @@ extern "C" fn _anal_call (user: *mut c_void, input: *const c_char) -> c_int {
     return 0;
 }
 
-const R_ANAL_PLUGIN: RCorePlugin = RCorePlugin {
-    name : MY_NAME,
+/*
+SHOULD BE DEPRECATED
+extern "C" fn _anal_getreg(_user: *mut c_void) -> *mut c_char {
+   let p = "=PC rip\n\
+  =BP rax\n\
+  =SP rsp\n\
+  =A0 rax\n\
+  =A1 rbx\n\
+  gpr rax .32 0 0\n\
+  gpr rbx .32 4 0\n\
+  gpr rip .32 8 0\n\
+  gpr rsp .32 12 0\n";
+unsafe {
+   return libc::strdup(p.as_ptr() as *const i8);
+}
+   // return b"\0" as *const u8 as *const c_char ;
+}
+*/
+
+extern "C" fn _anal_setreg(_user: *mut c_void) -> c_int {
+   let p = "=PC rip\n\
+  =BP rax\n\
+  =SP rsp\n\
+  =R0 rax\n\
+  =A0 rax\n\
+  =A1 rbx\n\
+  gpr rax .32 0 0\n\
+  gpr rbx .32 4 0\n\
+  gpr rip .32 8 0\n\
+  gpr rsp .32 12 0\n\0";
+unsafe {
+r_anal_set_reg_profile (_user, p.as_ptr() as *const i8);
+}
+   return 0;
+}
+
+const R_ANAL_PLUGIN: RAnalPlugin = RAnalPlugin {
+    name : MY_NAME as *const [u8] as *const c_char,
     desc : MY_DESC as *const [u8] as *const c_char,
     license : MY_LICENSE as *const [u8] as *const c_char,
+    arch: MY_ARCH as *const [u8] as *const c_char,
     author : MY_AUTHOR as *const [u8] as *const c_char,
     version : MY_VERSION as *const [u8] as * const c_char,
-    call: Some(_anal_call),
+    bits: 64,
+    esil: 1,
+    fileformat_type: 0,
     init: None,
-    deinit: None
+    fini: None,
+    archinfo: None,
+    anal_mask: None,
+    preludes: None,
+    op: Some(_anal_op),
+
+    cmd_ext: None,
+    diff_bb: None,
+    diff_eval: None,
+    diff_fcn: None,
+    esil_fini: None,
+    esil_init: None,
+    esil_trap: None,
+    esil_post_loop: None,
+    fingerprint_bb: None,
+    fingerprint_fcn: None,
+    get_reg_profile: None,
+    // get_reg_profile: Some(_anal_getreg),
+    // set_reg_profile: None,
+    set_reg_profile: Some(_anal_setreg),
 };
 
 #[no_mangle]
 #[allow(non_upper_case_globals)]
 pub static mut radare_plugin: RLibStruct = RLibStruct {
-    _type : RLibType::RLibTypeCore ,
-    data : ((&R_ANAL_PLUGIN) as *const RCorePlugin) as *const c_void,
+    _type : RLibType::RLibTypeAnal ,
+    data : ((&R_ANAL_PLUGIN) as *const RAnalPlugin) as *const c_void,
     version : R2_VERSION
 };
 
--- rust/analrs/src/radare2/mod.rs
+++ rust/analrs/src/radare2/mod.rs
@@ -2,72 +2,72 @@ use libc::*;
 use std::ffi::CStr;
 use std::ffi::{CString};
 
-pub const R_ANAL_OP_TYPE_COND: u64      = 0x80000000;
-pub const R_ANAL_OP_TYPE_REP: u64       = 0x40000000;
-pub const R_ANAL_OP_TYPE_MEM: u64       = 0x20000000; // TODO must be moved to prefix?
-pub const R_ANAL_OP_TYPE_REG: u64       = 0x10000000; // operand is a register
-pub const R_ANAL_OP_TYPE_IND: u64       = 0x08000000; // operand is indirect
-pub const R_ANAL_OP_TYPE_NULL: u64      = 0;
-pub const R_ANAL_OP_TYPE_JMP: u64       = 1;  /* mandatory jump */
-pub const R_ANAL_OP_TYPE_UJMP: u64      = 2;  /* unknown jump (register or so) */
-pub const R_ANAL_OP_TYPE_RJMP: u64      = R_ANAL_OP_TYPE_REG | R_ANAL_OP_TYPE_UJMP;
-pub const R_ANAL_OP_TYPE_IJMP: u64		= R_ANAL_OP_TYPE_IND | R_ANAL_OP_TYPE_UJMP;
-pub const R_ANAL_OP_TYPE_IRJMP: u64		= R_ANAL_OP_TYPE_IND | R_ANAL_OP_TYPE_REG | R_ANAL_OP_TYPE_UJMP;
-pub const R_ANAL_OP_TYPE_CJMP: u64		= R_ANAL_OP_TYPE_COND | R_ANAL_OP_TYPE_JMP;  /* conditional jump */
-pub const R_ANAL_OP_TYPE_MJMP: u64		= R_ANAL_OP_TYPE_MEM | R_ANAL_OP_TYPE_JMP;  /* conditional jump */
-pub const R_ANAL_OP_TYPE_UCJMP: u64		= R_ANAL_OP_TYPE_COND | R_ANAL_OP_TYPE_UJMP; /* conditional unknown jump */
-pub const R_ANAL_OP_TYPE_CALL: u64		= 3;  /* call to subroutine (branch+link) */
-pub const R_ANAL_OP_TYPE_UCALL: u64		= 4; /* unknown call (register or so) */
-pub const R_ANAL_OP_TYPE_RCALL: u64		= R_ANAL_OP_TYPE_REG | R_ANAL_OP_TYPE_UCALL;
-pub const R_ANAL_OP_TYPE_ICALL: u64		= R_ANAL_OP_TYPE_IND | R_ANAL_OP_TYPE_UCALL;
-pub const R_ANAL_OP_TYPE_IRCALL: u64	= R_ANAL_OP_TYPE_IND | R_ANAL_OP_TYPE_REG | R_ANAL_OP_TYPE_UCALL;
-pub const R_ANAL_OP_TYPE_CCALL: u64		= R_ANAL_OP_TYPE_COND | R_ANAL_OP_TYPE_CALL; /* conditional call to subroutine */
-pub const R_ANAL_OP_TYPE_UCCALL: u64	= R_ANAL_OP_TYPE_COND | R_ANAL_OP_TYPE_UCALL; /* conditional unknown call */
-pub const R_ANAL_OP_TYPE_RET: u64		= 5; /* returns from subroutine */
-pub const R_ANAL_OP_TYPE_CRET: u64		= R_ANAL_OP_TYPE_COND | R_ANAL_OP_TYPE_RET; /* conditional return from subroutine */
-pub const R_ANAL_OP_TYPE_ILL: u64		= 6;  /* illegal instruction // trap */
-pub const R_ANAL_OP_TYPE_UNK: u64		= 7; /* unknown opcode type */
-pub const R_ANAL_OP_TYPE_NOP: u64		= 8; /* does nothing */
-pub const R_ANAL_OP_TYPE_MOV: u64		= 9; /* register move */
-pub const R_ANAL_OP_TYPE_CMOV: u64		= 9 | R_ANAL_OP_TYPE_COND; /* conditional move */
-pub const R_ANAL_OP_TYPE_TRAP: u64		= 10; /* it's a trap! */
-pub const R_ANAL_OP_TYPE_SWI: u64		= 11;  /* syscall, software interrupt */
-pub const R_ANAL_OP_TYPE_UPUSH: u64		= 12; /* unknown push of data into stack */
-pub const R_ANAL_OP_TYPE_PUSH: u64		= 13;  /* push value into stack */
-pub const R_ANAL_OP_TYPE_POP: u64		= 14;   /* pop value from stack to register */
-pub const R_ANAL_OP_TYPE_CMP: u64		= 15;  /* compare something */
-pub const R_ANAL_OP_TYPE_ACMP: u64		= 16;  /* compare via and */
-pub const R_ANAL_OP_TYPE_ADD: u64		= 17;
-pub const R_ANAL_OP_TYPE_SUB: u64		= 18;
-pub const R_ANAL_OP_TYPE_IO: u64		= 19;
-pub const R_ANAL_OP_TYPE_MUL: u64		= 20;
-pub const R_ANAL_OP_TYPE_DIV: u64		= 21;
-pub const R_ANAL_OP_TYPE_SHR: u64		= 22;
-pub const R_ANAL_OP_TYPE_SHL: u64		= 23;
-pub const R_ANAL_OP_TYPE_SAL: u64		= 24;
-pub const R_ANAL_OP_TYPE_SAR: u64		= 25;
-pub const R_ANAL_OP_TYPE_OR: u64		= 26;
-pub const R_ANAL_OP_TYPE_AND: u64		= 27;
-pub const R_ANAL_OP_TYPE_XOR: u64		= 28;
-pub const R_ANAL_OP_TYPE_NOR: u64		= 29;
-pub const R_ANAL_OP_TYPE_NOT: u64		= 30;
-pub const R_ANAL_OP_TYPE_STORE: u64		= 31;  /* store from register to memory */
-pub const R_ANAL_OP_TYPE_LOAD: u64		= 32;  /* load from memory to register */
-pub const R_ANAL_OP_TYPE_LEA: u64		= 33; /* TODO add ulea */
-pub const R_ANAL_OP_TYPE_LEAVE: u64		= 34;
-pub const R_ANAL_OP_TYPE_ROR: u64		= 35;
-pub const R_ANAL_OP_TYPE_ROL: u64		= 36;
-pub const R_ANAL_OP_TYPE_XCHG: u64		= 37;
-pub const R_ANAL_OP_TYPE_MOD: u64		= 38;
-pub const R_ANAL_OP_TYPE_SWITCH: u64	= 39;
-pub const R_ANAL_OP_TYPE_CASE: u64		= 40;
-pub const R_ANAL_OP_TYPE_LENGTH: u64	= 41;
-pub const R_ANAL_OP_TYPE_CAST: u64		= 42;
-pub const R_ANAL_OP_TYPE_NEW: u64		= 43;
-pub const R_ANAL_OP_TYPE_ABS: u64		= 44;
-pub const R_ANAL_OP_TYPE_CPL: u64		= 45;	/* complement */
-pub const R_ANAL_OP_TYPE_CRYPTO: u64	= 46;
-pub const R_ANAL_OP_TYPE_SYNC: u64		= 47;
+pub const R_ANAL_OP_TYPE_COND: u32 = 0x80000000;
+pub const R_ANAL_OP_TYPE_REP: u32  = 0x40000000;
+pub const R_ANAL_OP_TYPE_MEM: u32  = 0x20000000; // TODO must be moved to prefix?
+pub const R_ANAL_OP_TYPE_REG: u32  = 0x10000000; // operand is a register
+pub const R_ANAL_OP_TYPE_IND: u32  = 0x08000000; // operand is indirect
+pub const R_ANAL_OP_TYPE_NULL: u32 = 0;
+pub const R_ANAL_OP_TYPE_JMP: u32  = 1;  /* mandatory jump */
+pub const R_ANAL_OP_TYPE_UJMP: u32 = 2;  /* unknown jump (register or so) */
+pub const R_ANAL_OP_TYPE_RJMP: u32 = R_ANAL_OP_TYPE_REG | R_ANAL_OP_TYPE_UJMP;
+pub const R_ANAL_OP_TYPE_IJMP: u32 = R_ANAL_OP_TYPE_IND | R_ANAL_OP_TYPE_UJMP;
+pub const R_ANAL_OP_TYPE_IRJMP: u32 = R_ANAL_OP_TYPE_IND | R_ANAL_OP_TYPE_REG | R_ANAL_OP_TYPE_UJMP;
+pub const R_ANAL_OP_TYPE_CJMP: u32  = R_ANAL_OP_TYPE_COND | R_ANAL_OP_TYPE_JMP;  /* conditional jump */
+pub const R_ANAL_OP_TYPE_MJMP: u32  = R_ANAL_OP_TYPE_MEM | R_ANAL_OP_TYPE_JMP;  /* conditional jump */
+pub const R_ANAL_OP_TYPE_UCJMP: u32 = R_ANAL_OP_TYPE_COND | R_ANAL_OP_TYPE_UJMP; /* conditional unknown jump */
+pub const R_ANAL_OP_TYPE_CALL: u32  = 3;  /* call to subroutine (branch+link) */
+pub const R_ANAL_OP_TYPE_UCALL: u32 = 4; /* unknown call (register or so) */
+pub const R_ANAL_OP_TYPE_RCALL: u32 = R_ANAL_OP_TYPE_REG | R_ANAL_OP_TYPE_UCALL;
+pub const R_ANAL_OP_TYPE_ICALL: u32 = R_ANAL_OP_TYPE_IND | R_ANAL_OP_TYPE_UCALL;
+pub const R_ANAL_OP_TYPE_IRCALL: u32 = R_ANAL_OP_TYPE_IND | R_ANAL_OP_TYPE_REG | R_ANAL_OP_TYPE_UCALL;
+pub const R_ANAL_OP_TYPE_CCALL: u32 = R_ANAL_OP_TYPE_COND | R_ANAL_OP_TYPE_CALL; /* conditional call to subroutine */
+pub const R_ANAL_OP_TYPE_UCCALL: u32 = R_ANAL_OP_TYPE_COND | R_ANAL_OP_TYPE_UCALL; /* conditional unknown call */
+pub const R_ANAL_OP_TYPE_RET: u32 = 5; /* returns from subroutine */
+pub const R_ANAL_OP_TYPE_CRET: u32 = R_ANAL_OP_TYPE_COND | R_ANAL_OP_TYPE_RET; /* conditional return from subroutine */
+pub const R_ANAL_OP_TYPE_ILL: u32 = 6;  /* illegal instruction // trap */
+pub const R_ANAL_OP_TYPE_UNK: u32 = 7; /* unknown opcode type */
+pub const R_ANAL_OP_TYPE_NOP: u32 = 8; /* does nothing */
+pub const R_ANAL_OP_TYPE_MOV: u32 = 9; /* register move */
+pub const R_ANAL_OP_TYPE_CMOV: u32		= 9 | R_ANAL_OP_TYPE_COND; /* conditional move */
+pub const R_ANAL_OP_TYPE_TRAP: u32		= 10; /* it's a trap! */
+pub const R_ANAL_OP_TYPE_SWI: u32		= 11;  /* syscall, software interrupt */
+pub const R_ANAL_OP_TYPE_UPUSH: u32		= 12; /* unknown push of data into stack */
+pub const R_ANAL_OP_TYPE_PUSH: u32		= 13;  /* push value into stack */
+pub const R_ANAL_OP_TYPE_POP: u32		= 14;   /* pop value from stack to register */
+pub const R_ANAL_OP_TYPE_CMP: u32		= 15;  /* compare something */
+pub const R_ANAL_OP_TYPE_ACMP: u32		= 16;  /* compare via and */
+pub const R_ANAL_OP_TYPE_ADD: u32		= 17;
+pub const R_ANAL_OP_TYPE_SUB: u32		= 18;
+pub const R_ANAL_OP_TYPE_IO: u32		= 19;
+pub const R_ANAL_OP_TYPE_MUL: u32		= 20;
+pub const R_ANAL_OP_TYPE_DIV: u32		= 21;
+pub const R_ANAL_OP_TYPE_SHR: u32		= 22;
+pub const R_ANAL_OP_TYPE_SHL: u32		= 23;
+pub const R_ANAL_OP_TYPE_SAL: u32		= 24;
+pub const R_ANAL_OP_TYPE_SAR: u32		= 25;
+pub const R_ANAL_OP_TYPE_OR: u32		= 26;
+pub const R_ANAL_OP_TYPE_AND: u32		= 27;
+pub const R_ANAL_OP_TYPE_XOR: u32		= 28;
+pub const R_ANAL_OP_TYPE_NOR: u32		= 29;
+pub const R_ANAL_OP_TYPE_NOT: u32		= 30;
+pub const R_ANAL_OP_TYPE_STORE: u32		= 31;  /* store from register to memory */
+pub const R_ANAL_OP_TYPE_LOAD: u32		= 32;  /* load from memory to register */
+pub const R_ANAL_OP_TYPE_LEA: u32		= 33; /* TODO add ulea */
+pub const R_ANAL_OP_TYPE_LEAVE: u32		= 34;
+pub const R_ANAL_OP_TYPE_ROR: u32		= 35;
+pub const R_ANAL_OP_TYPE_ROL: u32		= 36;
+pub const R_ANAL_OP_TYPE_XCHG: u32		= 37;
+pub const R_ANAL_OP_TYPE_MOD: u32		= 38;
+pub const R_ANAL_OP_TYPE_SWITCH: u32	= 39;
+pub const R_ANAL_OP_TYPE_CASE: u32		= 40;
+pub const R_ANAL_OP_TYPE_LENGTH: u32	= 41;
+pub const R_ANAL_OP_TYPE_CAST: u32		= 42;
+pub const R_ANAL_OP_TYPE_NEW: u32		= 43;
+pub const R_ANAL_OP_TYPE_ABS: u32		= 44;
+pub const R_ANAL_OP_TYPE_CPL: u32		= 45;	/* complement */
+pub const R_ANAL_OP_TYPE_CRYPTO: u32	= 46;
+pub const R_ANAL_OP_TYPE_SYNC: u32		= 47;
 
 #[repr(C)]
 pub enum RLibType {
@@ -79,14 +79,50 @@ pub enum RLibType {
     RLibTypeParse = 5,
     RLibTypeBin = 6,
     RLibTypeBinXtr = 7,
-    RLibTypeBp = 8,
-    RLibTypeSyscall = 9,
-    RLibTypeFastcall = 10,
-    RLibTypeCrypto = 11,
-    RLibTypeCore = 12,
-    RLibTypeEgg = 13,
-    RLibTypeFs = 14,
-    RLibTypeLast = 15,
+    RLibTypeBinLdr = 8,
+    RLibTypeBp = 9,
+    RLibTypeSyscall = 10,
+    RLibTypeFastcall = 11,
+    RLibTypeCrypto = 12,
+    RLibTypeCore = 13,
+    RLibTypeEgg = 14,
+    RLibTypeFs = 15,
+    RLibTypeEsil = 16,
+    RLibTypeLast = 17,
+}
+
+#[repr(C)]
+// #[derive(Default)]
+pub struct RAnalPlugin {
+    pub name: *const c_char,
+    pub desc: *const c_char,
+    pub license: *const c_char,
+    pub arch: *const c_char,
+    pub author: *const c_char,
+    pub version: *const c_char,
+    pub bits: c_int,
+    pub esil: c_int,
+    pub fileformat_type: c_int,
+
+    pub init: Option<extern "C" fn(*mut c_void) -> c_int>,
+    pub fini: Option<extern "C" fn(*mut c_void) -> c_int>,
+    pub archinfo: Option<extern "C" fn(*mut c_void, usize) -> c_int>,
+    pub anal_mask: Option<extern "C" fn(*mut c_void, *mut c_void, usize) -> c_int>,
+    pub preludes: Option<extern "C" fn(*mut c_void) -> *mut c_void>,
+
+    pub op: Option<extern "C" fn(user: *mut c_void, op: *mut RAnalOp, addr: u64, data: *mut c_void, len: usize, mask: usize) -> c_int>,
+    pub cmd_ext: Option<extern "C" fn(*mut c_void) -> *mut c_void>,
+    pub set_reg_profile: Option<extern "C" fn(*mut c_void) -> c_int>,
+    pub get_reg_profile: Option<extern "C" fn(*mut c_void) -> *mut c_char>,
+    pub fingerprint_bb: Option<extern "C" fn(*mut c_void) -> *mut c_void>,
+    pub fingerprint_fcn: Option<extern "C" fn(*mut c_void) -> *mut c_void>,
+    pub diff_bb: Option<extern "C" fn(*mut c_void) -> *mut c_void>,
+    pub diff_fcn: Option<extern "C" fn(*mut c_void) -> *mut c_void>,
+    pub diff_eval: Option<extern "C" fn(*mut c_void) -> *mut c_void>,
+    pub esil_init: Option<extern "C" fn(*mut c_void) -> *mut c_void>,
+    pub esil_post_loop: Option<extern "C" fn(*mut c_void) -> *mut c_void>,
+    pub esil_trap: Option<extern "C" fn(*mut c_void) -> *mut c_void>,
+    pub esil_fini: Option<extern "C" fn(*mut c_void) -> *mut c_void>,
 }
 
 #[repr(C)]
@@ -178,10 +214,9 @@ pub struct RAnalSwitchOp {
 pub struct RAnalOp {
     pub mnemonic: *mut c_char,
     pub addr: u64,
-    pub _type: u64,
-    pub prefix: u64,
-    pub type2: u64,
-    pub group: c_int,
+    pub _type: u32,
+    pub prefix: c_int,
+    pub type2: u32,
     pub stackop: c_int,
     pub cond: c_int,
     pub size: c_int,
@@ -191,6 +226,7 @@ pub struct RAnalOp {
     pub family: c_int,
     pub id: c_int,
     pub eob: bool,
+    pub sign: bool,
     pub delay: c_int,
     pub jump: u64,
     pub fail: u64,
@@ -246,8 +282,10 @@ extern {
     pub fn r_core_is_valid_offset (core: *mut c_void, offset: u64) -> c_int;
     pub fn r_core_cmdf (core: *mut c_void, format: *const c_char, ...) -> c_int;
     pub fn r_core_cmd (core: *mut c_void, cstr: *const c_char, log: c_int) -> c_int;
-    pub fn r_cons_print(cstr: *const c_char) -> c_void;
-    pub fn r_cons_strcat(cstr: *const c_char) -> c_void;
+    pub fn r_cons_print (cstr: *const c_char) -> c_void;
+    pub fn r_cons_strcat (cstr: *const c_char) -> c_void;
+    pub fn r_reg_set_profile_string (reg: *const c_void, cstr: *const c_char) -> c_void;
+    pub fn r_anal_set_reg_profile (anal: *const c_void, cstr: *const c_char) -> c_int;
 }
 
 pub fn r2_cmd(core: *mut c_void, cmd: &str) -> &str {
