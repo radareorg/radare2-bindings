5ee9d84d263b1e15afd2cb6cfab110cc06ba4a76
lazymio <mio@lazym.io>
2021-11-15 11:26:51 +0100

    Bump 5.5.0

--- r2libr/setup.py
+++ r2libr/setup.py
@@ -223,7 +223,7 @@ with open(Path(ROOT_DIR) / "README.md", "r+") as f:
 
 setuptools.setup(
     name="r2libr",
-    version="5.4.2",
+    version="5.5.0",
     author="mio",
     author_email="mio@lazym.io",
     description="Yet anohter radare2 python bindings.",
