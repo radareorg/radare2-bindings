9fcf43d97717e5345d01f8ee754ffc2d0f6b4771
pancake <pancake@nopcode.org>
2023-05-11 11:13:10 +0200

    cargo fmt

--- rust/r2api/build.rs
+++ rust/r2api/build.rs
@@ -1,4 +1,3 @@
-
 extern crate bindgen;
 
 use std::env;
@@ -9,29 +8,29 @@ fn main() {
     // Tell cargo to statically link to the radare2-build static lib
     #[cfg(feature = "static")]
     {
-    // TODO: this ../radare2-bild path should be dynamically constructed
-    // println!("cargo:rustc-link-search=../radare2-build/radare2/libr/");
-    // println!("cargo:rustc-link-lib=r");
-    println!("cargo:rustc-link-arg=../radare2-build/radare2/libr/libr.a");
+        // TODO: this ../radare2-bild path should be dynamically constructed
+        // println!("cargo:rustc-link-search=../radare2-build/radare2/libr/");
+        // println!("cargo:rustc-link-lib=r");
+        println!("cargo:rustc-link-arg=../radare2-build/radare2/libr/libr.a");
     }
     #[cfg(not(feature = "static"))]
     {
-    println!("cargo:rustc-link-lib=r_io");
-    println!("cargo:rustc-link-lib=r_asm");
-    println!("cargo:rustc-link-lib=r_arch");
-    println!("cargo:rustc-link-lib=r_esil");
-    println!("cargo:rustc-link-lib=r_anal");
-    println!("cargo:rustc-link-lib=r_search");
-    println!("cargo:rustc-link-lib=r_util");
-    println!("cargo:rustc-link-lib=r_reg");
-    println!("cargo:rustc-link-lib=r_debug");
-    println!("cargo:rustc-link-lib=r_lang");
-    println!("cargo:rustc-link-lib=r_bin");
-    println!("cargo:rustc-link-lib=r_syscall");
-    println!("cargo:rustc-link-lib=r_core");
-    println!("cargo:rustc-link-lib=r_socket");
-    println!("cargo:rustc-link-lib=r_fs");
-    println!("cargo:rustc-link-lib=r_cons");
+        println!("cargo:rustc-link-lib=r_io");
+        println!("cargo:rustc-link-lib=r_asm");
+        println!("cargo:rustc-link-lib=r_arch");
+        println!("cargo:rustc-link-lib=r_esil");
+        println!("cargo:rustc-link-lib=r_anal");
+        println!("cargo:rustc-link-lib=r_search");
+        println!("cargo:rustc-link-lib=r_util");
+        println!("cargo:rustc-link-lib=r_reg");
+        println!("cargo:rustc-link-lib=r_debug");
+        println!("cargo:rustc-link-lib=r_lang");
+        println!("cargo:rustc-link-lib=r_bin");
+        println!("cargo:rustc-link-lib=r_syscall");
+        println!("cargo:rustc-link-lib=r_core");
+        println!("cargo:rustc-link-lib=r_socket");
+        println!("cargo:rustc-link-lib=r_fs");
+        println!("cargo:rustc-link-lib=r_cons");
     }
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
--- rust/r2api/src/bin/example.rs
+++ rust/r2api/src/bin/example.rs
@@ -1,25 +1,23 @@
-use r2api::{r_core_new,r_core_cmd0,r_cons_flush,r_core_free, r_io_new};
+use r2api::{r_cons_flush, r_core_cmd0, r_core_free, r_core_new, r_io_new};
 use std::ffi::CString;
 
 macro_rules! c_str {
     ($lit:expr) => {
         std::ffi::CStr::from_ptr(concat!($lit, "\0").as_ptr() as *const i8).as_ptr()
-    }
+    };
 }
 
-
-
 fn main() {
-	/*
-	let c = r_core::new();
-	c.cmd("?e hello world");
-	c.free();
-	*/
+    /*
+    let c = r_core::new();
+    c.cmd("?e hello world");
+    c.free();
+    */
 
     unsafe {
         let c = r_core_new();
         let pangram = "?E Hello World\x00".as_ptr() as *const i8;
-	let _ = r_io_new ();
+        let _ = r_io_new();
         r_core_cmd0(c, pangram);
         r_core_cmd0(c, CString::new("b").unwrap().as_ptr());
         r_core_cmd0(c, c_str!("?e woot"));
